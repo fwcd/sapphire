9b57afe4c9f85d97ad8fbca6330bfd1a9aef5c61
fwcd <fwcdmail@gmail.com>
2018-02-03 00:22:03 +0100

    Cleaned up some demo classes

--- src/com/fwcd/amethyst/scene/DemoRotation.java
+++
@@ -1,8 +0,0 @@
-package com.fwcd.amethyst.scene;
-
-public class DemoRotation implements EntityBehavior {
-	@Override
-	public void onTick(Entity entity, Scene scene) {
-		entity.rotate(0, 1, 0);
-	}
-}
